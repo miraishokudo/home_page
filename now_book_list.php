72ff7d3073d1125ba0261c19f02e7fc7654ba494
miraishokudo <miraishokudo@gmail.com>
2014-08-30 12:02:33 +0900

    Add indent

--- now_book_list.php
+++ now_book_list.php
@@ -2,26 +2,26 @@
 	<head>
    <link rel="stylesheet" href="assets/css/style.css">
 	</head>
- <body><div class='portfolio-message mid_font'>
+	<body>
+		<div class='portfolio-message mid_font'>
+	<?php
+	 	require_once 'php/hatena_scraping.php';
 
- <?php
- 	require_once 'php/hatena_scraping.php';
+	 	$start_day = new DateTime('2014-08-03');
+	 	$today = new DateTime();
+	 	$interval = $today->diff($start_day)->format('%a');
+	 	$interval_to_first_sunday = floor($interval/14)*14;
+	 	$interval_to_last_saturday = $interval_to_first_sunday + 13;
 
- 	$start_day = new DateTime('2014-08-03');
- 	$today = new DateTime();
- 	$interval = $today->diff($start_day)->format('%a');
- 	$interval_to_first_sunday = floor($interval/14)*14;
- 	$interval_to_last_saturday = $interval_to_first_sunday + 13;
+	 	$book_week_start_day = new DateTime('2014-08-03');
+		$book_week_start_day->add(new DateInterval("P{$interval_to_first_sunday}D"));
 
- 	$book_week_start_day = new DateTime('2014-08-03');
-	$book_week_start_day->add(new DateInterval("P{$interval_to_first_sunday}D"));
+	 	$book_week_end_day = new DateTime('2014-08-03');
+		$book_week_end_day->add(new DateInterval("P{$interval_to_last_saturday}D"));
 
- 	$book_week_end_day = new DateTime('2014-08-03');
-	$book_week_end_day->add(new DateInterval("P{$interval_to_last_saturday}D"));
-
-  echo("■{$book_week_start_day->format('Y/m/d')} 〜 {$book_week_end_day->format('Y/m/d')} の本棚<br/>");
-  echo(todays_book_list());
-?>
+	  echo("■{$book_week_start_day->format('Y/m/d')} 〜 {$book_week_end_day->format('Y/m/d')} の本棚<br/>");
+	  echo(todays_book_list());
+	?>
 		</div>
   	<script src="assets/js/iframe_height.js"></script>
 	</body>
