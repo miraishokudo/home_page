128b384a753140d402a79af556577512949abbfc
miraishokudo <miraishokudo@gmail.com>
2015-01-16 15:11:34 +0900

    Refact indents

--- index.php
+++ index.php
@@ -159,24 +159,24 @@
             </div>
 
 <?php
-    require_once 'php/hatena_scraping.php';
-    $start_day = new DateTime('2014-08-03');
-    $today = new DateTime();
-    $interval = $today->diff($start_day)->format('%a');
-    $interval_to_first_sunday = floor($interval/14)*14;
-    $interval_to_last_saturday = $interval_to_first_sunday + 13;
+require_once 'php/hatena_scraping.php';
+$start_day = new DateTime('2014-08-03');
+$today = new DateTime();
+$interval = $today->diff($start_day)->format('%a');
+$interval_to_first_sunday = floor($interval/14)*14;
+$interval_to_last_saturday = $interval_to_first_sunday + 13;
 
-    $book_week_start_day = new DateTime('2014-08-03');
-    $book_week_start_day->add(new DateInterval("P{$interval_to_first_sunday}D"));
+$book_week_start_day = new DateTime('2014-08-03');
+$book_week_start_day->add(new DateInterval("P{$interval_to_first_sunday}D"));
 
-    $book_week_end_day = new DateTime('2014-08-03');
-    $book_week_end_day->add(new DateInterval("P{$interval_to_last_saturday}D"));
+$book_week_end_day = new DateTime('2014-08-03');
+$book_week_end_day->add(new DateInterval("P{$interval_to_last_saturday}D"));
 
-  echo("■{$book_week_start_day->format('Y/m/d')} 〜 {$book_week_end_day->format('Y/m/d')} の本棚（背表紙の色で4冊づつ分けています）<br/>");
-        <div class='portfolio-message mid_font'>
-            echo(todays_book_title_and_photo());
-        </div>
+echo("■{$book_week_start_day->format('Y/m/d')} 〜 {$book_week_end_day->format('Y/m/d')} の本棚（背表紙の色で4冊づつ分けています）<br/>");
 ?>
+            <div class='portfolio-message mid_font'>
+<?php echo(todays_book_title_and_photo()); ?>
+            </div>
         </div>
         <!-- <div class="presentation container" id='kinka'>
             <div class="portfolio-title">
