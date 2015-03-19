d70e8705885e353c3f936c73a558debdac8fc295
sekai <miraishokudo@gmail.com>
2015-03-19 16:23:07 +0900

    Add viewport

--- php/book_info.php
+++ php/book_info.php
@@ -18,6 +18,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/style.css">
     </head>
     <body>
