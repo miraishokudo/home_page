d0c310855385dbb92eee277d595e07e2ea27a37d
sekai <miraishokudo@gmail.com>
2015-04-17 12:25:09 +0900

    Change book_title_design

--- php/book_info.php
+++ php/book_info.php
@@ -26,9 +26,9 @@
         <div id='book_list_title'>
             ■<?=$book_week_start_day?> 〜 <?=$book_week_end_day?>の本棚（背表紙の色で4冊づつ分けています）
         </div>
-        <div><?=$books_src?></div>
+        <div class='book_title'><?=$books_src?></div>
         <script src="../js/auto_height.js"></script>
-        <script src="assets/js/jquery-1.8.2.min.js"></script>
-        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     </body>
 </html>
