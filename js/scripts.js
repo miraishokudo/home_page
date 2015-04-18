815f7ecb50bc604adf7dbec1ab3954eed1b9bc1d
sekai <miraishokudo@gmail.com>
2015-04-18 09:41:54 +0900

    Add smooth scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
-/*
-    Slider
-*/
 $(document).ready(function() {
   $('.flexslider').flexslider({
       animation: "slide"
   });
+});
+
+$(function(){
+   $('a[href^=#]').click(function() {
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $('body,html').animate({scrollTop:position}, 500, 'swing');
+      false;
+   });
 });
