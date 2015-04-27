6478febca6eff0ba6921e4339f04c30ba887fc0b
sekai <miraishokudo@gmail.com>
2015-04-27 09:42:44 +0900

    id要素へのリンク押下時に固定ヘッダ高-ボーダー高（$(header.headshrunk).height() - 2）分だけリンクがずれるため、今まではid要素を期待するリンクより上側に付けることでずれを回避していたが、id要素を期待するリンクに付与し、js側で移動アニメーション幅を調整することでずれを回避するように変更. Aたぐ要素がせいせいされるタイミングは「初期表示」「header縮小時」「navメニュー表示時」の3箇所あるため、これら3箇所でずれ回避ロジックを追加

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,3 @@ $(document).ready(function() {
       animation: "slide"
   });
 });
-
-$(function(){
-   $('a[href^=#]').click(function() {
-      var href= $(this).attr("href");
-      var _target = $(href);
-
-      if (href != '#nav' && (typeof _target !== "undefined") && (typeof _target.offset() !== "undefined")) {
-        var position = _target.offset().top;
-        $('body,html').animate({scrollTop:position}, 500, 'swing');
-        false;
-      }
-   });
-});
