7437708943571113c813d7afa9e78a756aeb9ad0
sekai <miraishokudo@gmail.com>
2015-04-30 13:52:09 +0900

    開業予定日を追加

--- about_miraitosho.php
+++ about_miraitosho.php
@@ -17,7 +17,8 @@
 		<iframe id='tosho_calender'style="border-width: 0; margin: 0 5px 30px;" src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=300&amp;wkst=1&amp;bgcolor=%23cccccc&amp;src=miraishokudo%40gmail.com&amp;color=%232952A3&amp;ctz=Asia%2FTokyo"height="220" width="340"frameborder="0" scrolling="no" align="top"></iframe>
 		<div class='mid_font'><?=$title_src?></div><br/>
 		<div><?=$photo_src?></div><div style="clear:both;"></div><br/><br/>
-		各記事のタイトル【○色の本】は背表紙の色です。<br /> <a href="http://miraitosho.hateblo.jp/entry/2014/04/22/225652" target="_blank">蔵書一覧はこちら</a><br /> 　<br /> 未来図書のテーマは、”知らない世界も面白い”です。<br /> 新しい世界、気になる本が見つかれば幸いです。<br /> 　<br /> ※<a href="http://miraishokudo.com" target="_blank">未来食堂</a>は2015年末OPENの予定です。<br /> おまたせしております、今しばらくお待ちください。
+		各記事のタイトル【○色の本】は背表紙の色です。<br /> <a href="http://miraitosho.hateblo.jp/entry/2014/04/22/225652" target="_blank">蔵書一覧はこちら</a><br /> 　<br /> 未来図書のテーマは、”知らない世界も面白い”です。<br /> 新しい世界、気になる本が見つかれば幸いです。<br /> 　<br /> 
+		※2015年7月OPEN予定。神保町徒歩3分。<a href='https://www.google.co.jp/maps/place/%E6%97%A5%E6%9C%AC%E6%95%99%E8%82%B2%E4%BC%9A%E9%A4%A8/@35.694342,139.756234,17z/data=!3m1!4b1!4m2!3m1!1s0x60188c13b8af0a3f:0x9c26ea740eb79b4d' target="_blank">千代田区一ツ橋2−6−2 日本教育会館様B1</a>
 	</body>
 </html>
 
