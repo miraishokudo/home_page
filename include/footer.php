760df1a0156235e26bf1d052d166f5b8044a5553
sekai <miraishokudo@gmail.com>
2015-06-10 18:54:15 +0900

    サブページのヘッダー・フッターを共通化

---
+++ include/footer.php
@@ -0,0 +1,25 @@
+<footer>
+    <div class="container text-left">
+        <div class="row">
+            <div class="widget col-xs-12 col-sm-3 small_font">
+                <p>住所 :<br/>東京都千代田区一ツ橋2-6-2 <br class='hidden-xs'/>日本教育会館 B1</p>
+                <p>営業時間 :<br/>9〜22時（日曜休,月火は14時迄）<br/>※本の閲覧は平日ランチタイム以外でお願いします</p>
+                <p>電話 :<br/>03-6905-7147</p><p>mail :<br/><A HREF="mailto:miraishokudo@gmail.com">miraishokudo@gmail.com</a></p>
+            </div>
+            <div class="widget col-xs-12 col-sm-4 small_font">
+                <p>アクセス :<br/>
+                半蔵門線神保町駅（A1出口）徒歩3分<br/>
+                三田線神保町駅（A1出口）徒歩5分<br/>
+                東西線竹橋駅（1b出口）徒歩5分<br/>
+                東西線九段下駅（6番出口）徒歩7分</p>
+                <a href="http://www.jec.or.jp/koutuu/" target='_brank'>日本教育会館様 交通案内地図</a>
+            </div>
+            <div class="widget col-xs-12 col-sm-5 map">
+            <a href='https://www.google.co.jp/maps/place/%E6%97%A5%E6%9C%AC%E6%95%99%E8%82%B2%E4%BC%9A%E9%A4%A8/@35.694342,139.756234,17z/data=!3m1!4b1!4m2!3m1!1s0x60188c13b8af0a3f:0x9c26ea740eb79b4d' target="_blank">
+                <img src="../img/access_map.png" width="100%">
+            </a>
+            <div class='visible-xs' style="height: 20px;"></div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ include/header.php
@@ -0,0 +1,16 @@
+
+            <div class="header">
+                <div class="navbar">
+                    <div class="navbar-inner">
+                        <div class='row'>
+                            <a href="../index.html" class='pull-left'><img src="../img/logo.png" alt="Logo" /></a>
+                            <div class='access mid_font text-left pull-right'>
+                                 <a href='https://www.google.co.jp/maps/place/%E6%97%A5%E6%9C%AC%E6%95%99%E8%82%B2%E4%BC%9A%E9%A4%A8/@35.694342,139.756234,17z/data=!3m1!4b1!4m2!3m1!1s0x60188c13b8af0a3f:0x9c26ea740eb79b4d' target="_blank">千代田区一ツ橋2-6-2&nbsp;&nbsp;日本教育会館B1</a><br/>
+                                9〜22時（日曜休,月火は14時迄）<br/>
+                                神保町駅徒歩3分&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;03-6905-7147
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='visible-xs' style="height:40px;"></div>
