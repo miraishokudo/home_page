4e8386104307216ce74d22eedaa885f32e9ea261
sekai <miraishokudo@gmail.com>
2015-08-03 06:56:57 +0900

    安見に祝日を追加

--- include/header.php
+++ include/header.php
@@ -6,7 +6,7 @@
                             <a href="../index.html" class='pull-left'><img src="../img/logo.png" alt="Logo" /></a>
                             <div class='access mid_font text-left pull-right'>
                                  <a href='https://www.google.co.jp/maps/place/%E6%97%A5%E6%9C%AC%E6%95%99%E8%82%B2%E4%BC%9A%E9%A4%A8/@35.694342,139.756234,17z/data=!3m1!4b1!4m2!3m1!1s0x60188c13b8af0a3f:0x9c26ea740eb79b4d' target="_blank">千代田区一ツ橋2-6-2&nbsp;&nbsp;日本教育会館B1</a><br/>
-                                11〜22時（日曜休,月火は14時迄）<br/>
+                                11〜22時（日祝休,月火は14時迄）<br/>
                                 神保町駅徒歩3分&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;03-6905-7147
                             </div>
                         </div>
