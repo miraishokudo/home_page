a339fde9576db4e9ce38867459d4e7625d6d6bfc
sekai <miraishokudo@gmail.com>
2015-08-03 00:44:28 +0900

    windowリサイズ時に本日の未来図書一覧もリサイズされるようイベントリスナーを追加。setTimerを付け不要に何度も呼び出される事を抑止

--- js/auto_height.js
+++ js/auto_height.js
@@ -3,3 +3,14 @@ function iframeResize(){
 	window.parent.document.getElementById('iframe_book_info').style.height = PageHight + 'px'; // iframeの高さを変更
 }
 window.onload = iframeResize;
+
+var timer = false;
+//jqueryを読み込んでいないため素の関数
+window.addEventListener('resize', function () {
+    if (timer !== false) {
+        clearTimeout(timer);
+    }
+    timer = setTimeout(function() {
+        iframeResize();
+    }, 200);
+});
